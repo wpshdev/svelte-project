f0cc44e252fc9d5a603c8eb7930616ef7db9b7ad
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-01-20 21:11:25 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>    
+</main>
+
+<style lang="scss">
+    main{
+        h1{
+            color: red;
+        }
+        p{
+            font-weight: 900;
+        }
+    }
+</style>
