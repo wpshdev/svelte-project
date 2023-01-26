5efb2d0dd3c9130b874ac3423818baeea98d47a5
parthwps <parth@wpsuperheroes.com>
2023-01-26 23:40:13 +0530

    Some layout changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,21 @@
 		<h1>WE BUILD SERENE<br>DREAMS</h1>
 	</div>
 </section>
+<style lang="scss">
+.aerialvideo{
+    background: url("src/lib/img/slide1.jpg");
+    min-height: 100vh;
+    .text-block{
+        position: absolute;
+        color: #fff;
+        bottom: 5rem;
+        left: 5rem;
+        padding: 3rem;
+        background-color: rgba(0,0,0,0.4);
+    }
+    h1{
+        font-size: 4rem;
+        font-weight: 700;
+    }
+}
+</style>
