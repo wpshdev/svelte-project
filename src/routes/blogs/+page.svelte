76477d4b3eac163e4f21a0c78cffbc57ec55f097
parthwps <parth@wpsuperheroes.com>
2023-02-14 17:03:07 +0530

    blogs_page api connected

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import { Col, Container, Row } from 'sveltestrap';
     import logo from '$lib/img/logo.svg';
+    export let data;
+    let blogs = data.data;
+    let url = "https://strapi.ulfbuilt.com:1337";
+    console.log(data);
+    // let title = data.data[0].attributes.title;
 </script>
 <svelte:head>
 	<title>News/Blogs</title>
@@ -11,47 +16,14 @@
         <Col md="12"><h1 class="text-center">BLOG</h1></Col>
     </Row>
     <Row>
+        {#each blogs as blog}
         <Col md="4">
             <a href="/blog" class="text-decoration-none text-black">
-                <img src={logo} alt="blogtitle" class="w-100">
-                <h4 class="text-center">Blog Title</h4>
-                <h4 class="text-center">Blog description</h4>
+                <img src="{url+blog.attributes.featuredimage.data.attributes.formats.small.url}" alt="blogtitle" class="w-100">
+                <h4 class="text-center">{blog.attributes.title}</h4>
+                <p class="text-center">{blog.attributes.shorttext}</p>
             </a>
         </Col>
-        <Col md="4">
-            <a href="/blog" class="text-decoration-none text-black">
-                <img src={logo} alt="blogtitle" class="w-100">
-                <h4 class="text-center">Blog Title</h4>
-                <h4 class="text-center">Blog description</h4>
-            </a>
-        </Col>
-        <Col md="4">
-            <a href="/blog" class="text-decoration-none text-black">
-                <img src={logo} alt="blogtitle" class="w-100">
-                <h4 class="text-center">Blog Title</h4>
-                <h4 class="text-center">Blog description</h4>
-            </a>
-        </Col>
-        <Col md="4">
-            <a href="/blog" class="text-decoration-none text-black">
-                <img src={logo} alt="blogtitle" class="w-100">
-                <h4 class="text-center">Blog Title</h4>
-                <h4 class="text-center">Blog description</h4>
-            </a>
-        </Col>
-        <Col md="4">
-            <a href="/blog" class="text-decoration-none text-black">
-                <img src={logo} alt="blogtitle" class="w-100">
-                <h4 class="text-center">Blog Title</h4>
-                <h4 class="text-center">Blog description</h4>
-            </a>
-        </Col>
-        <Col md="4">
-            <a href="/blog" class="text-decoration-none text-black">
-                <img src={logo} alt="blogtitle" class="w-100">
-                <h4 class="text-center">Blog Title</h4>
-                <h4 class="text-center">Blog description</h4>
-            </a>
-        </Col>
+        {/each}
     </Row>
 </Container>
