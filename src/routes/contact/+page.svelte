66387f79c3579942ab25bcd27e7854934263a2de
parthwps <parth@wpsuperheroes.com>
2023-02-22 19:59:00 +0530

    contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,18 +1,64 @@
 <script lang="ts">
+    export let data;
     import { Form, FormGroup, Input, Label } from 'sveltestrap';
     import { Button } from 'sveltestrap';
     const color = 'primary';
     import { Col, Container, Row } from 'sveltestrap';
+    console.log(data);
+    let featuredimage = data.data.attributes.featuredimage.data.attributes.url;
+    let phone = data.data.attributes.phone;
+    let fax = data.data.attributes.fax;
+    let office_address = data.data.attributes.office_address;
+    let mailing_address = data.data.attributes.mailing_address;
+    let map_url = data.data.attributes.map_url;
+    let facebook = data.data.attributes.facebook;
+    let twitter = data.data.attributes.twitter;
+    let youtube = data.data.attributes.youtube;
+    let houzz = data.data.attributes.youtube;
+    let name = ''
+    let email = ''
+    let subject = ''
+    let message = ''
+    let result = ''
+    async function doContact () {
+        const url = 'https://strapi.ulfbuilt.com:1337/api/contact-forms';
+		const res = await fetch(url, {
+			method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'Authorization': 'bearer  ec0d6b5aece1773cbd6e5f48756c70d9b0b3a59a4d1c325a2e699c1c1b1cae0980dc56aa2c3dfd565237b2a00db9a547a1a9e54a86f80697b31766e6bf80257b37760df84c70b534edeb4df0bdde9452777a52a757850d7a82c28dba854776c405f20ef3fbd95c72b759280f375f69191f2ca75d69600ea9584d8b2100309072' },
+			body: JSON.stringify({
+                data:{
+                "name": name,
+                "email": email,
+                "subject": subject,
+                "message": message
+                }
+			})
+		})
+		const json = await res.json()
+        if(json.error){
+            result = json.error.message
+        }else{
+            result = 'Thank you for contacting us. We will reach you soon.'
+        }
+	}
 </script>
 <svelte:head>
 	<title>Contact us</title>
 	<meta name="description" content="ULF BUILT" />
 </svelte:head>
-<div class="contact-1 container-fluid">
+<div class="contact-1 container-fluid" style="background-image:url(https://strapi.ulfbuilt.com:1337{featuredimage})">
     <Container>
         <Row>
-            <Col md="4"></Col>
-            <div class="contact-form col-md-4">
+            <Col md="4" style="text-align:right;padding-top: 150px;padding-right: 50px;color: #fff;">
+            <h3>PHONE</h3>
+            <p>{phone}</p>
+            <h3>FAX</h3>
+            <p>{fax}</p>
+            <h3>OUR OFFICE</h3>
+            <p><a href="{map_url}" target="_blank" rel="noreferrer">{office_address}</a></p>
+            <h3><a href="{map_url}" target="_blank" rel="noreferrer">Map to our office</a></h3>
+            </Col>
+            <Col md="4" class="contact-form">
                 <h2>GET IN TOUCH WITH US</h2>
                 <p>We are here to provide you with more info, and answer any questions you may have.</p>
                 <Form method="post">
@@ -32,26 +78,22 @@
                         <Label for="yourMessage">Your Message</Label>
                         <Input type="textarea" id="yourMessage" placeholder="Your Message"/>
                     </FormGroup>
-                    <Button {color}>Send</Button>
+                    <Button type="button" {color} on:click={doContact}>Send</Button>
                 </Form>
-            </div>
-            <Col md="4"></Col>
+                {result}
+            </Col>
+            <Col md="4" style="padding-top: 200px;padding-left: 50px;color: #fff;">
+            <h3>Mailing Address</h3>
+            <p>{mailing_address}</p>
+            <h3>Find us on</h3>
+            <p><a href="{facebook}">Facebook</a>, <a href="{twitter}">Twitter</a>, <a href="{youtube}">Youtube</a></p>
+            <h3><a href="{houzz}">Houzz</a></h3>
+            </Col>
         </Row>
     </Container>
 </div>
 <style lang="scss">
-.contact-1{
-    background: url("$lib/img/ULFBUILT-TEAM-1.jpg");
-    min-height: 100vh;
-    background-position: bottom;
-    background-size: cover;
-    padding-top: 150px;
-
-    .contact-form{
-        background-color: rgba(0,0,0,0.6);
-        padding: 50px 20px;
-        margin:50px 0px;
-        color: #fff;
+    a,a:hover{
+        color:#fff;
     }
-}
 </style>
