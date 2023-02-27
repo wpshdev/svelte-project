69c1316f967000e03c9b219dcce985042ea1b3ac
paulwpsh <113500108+paulwpsh@users.noreply.github.com>
2023-02-28 01:19:45 +0800

    changed port of localhost

--- src/routes/our-team/+page.ts
+++ src/routes/our-team/+page.ts
@@ -4,6 +4,6 @@ export async function load() {
     const headers = {
         Authorization: 'Bearer ec0d6b5aece1773cbd6e5f48756c70d9b0b3a59a4d1c325a2e699c1c1b1cae0980dc56aa2c3dfd565237b2a00db9a547a1a9e54a86f80697b31766e6bf80257b37760df84c70b534edeb4df0bdde9452777a52a757850d7a82c28dba854776c405f20ef3fbd95c72b759280f375f69191f2ca75d69600ea9584d8b2100309072' 
     }
-    const response = await axios.get("localhost:3001/api/our-team?populate=deep", { headers });
+    const response = await axios.get("localhost:1337/api/our-team?populate=deep", { headers });
     return response.data;
 }                                                                     
