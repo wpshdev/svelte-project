f1508b3c64880014942546c2131a8ce8a66a86c7
paulwpsh <113500108+paulwpsh@users.noreply.github.com>
2023-02-28 01:30:48 +0800

    changed localhost to url again

--- src/routes/our-team/[slug]/+page.ts
+++ src/routes/our-team/[slug]/+page.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function load(params: { params: { slug: string; }; }) {
-    const url = 'localhost:1337/api/team-members?filters[slug][$eq]='+params.params.slug+'&populate=deep';
+    const url = 'https://strapi.ulfbuilt.com:1337/api/team-members?filters[slug][$eq]='+params.params.slug+'&populate=deep';
     const headers = {
         Authorization: 'Bearer ec0d6b5aece1773cbd6e5f48756c70d9b0b3a59a4d1c325a2e699c1c1b1cae0980dc56aa2c3dfd565237b2a00db9a547a1a9e54a86f80697b31766e6bf80257b37760df84c70b534edeb4df0bdde9452777a52a757850d7a82c28dba854776c405f20ef3fbd95c72b759280f375f69191f2ca75d69600ea9584d8b2100309072' 
     }
