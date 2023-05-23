d976cceb41fe90507878485e91f60978361f76fa
parthwps <parth@wpsuperheroes.com>
2023-05-23 21:46:53 +0530

    minify

---
+++ src/hooks.server.js
@@ -0,0 +1,20 @@
+import { minify } from 'html-minifier-terser'
+
+const minification_options = {
+  collapseWhitespace: true,
+  collapseInlineTagWhitespace: true,
+  removeComments: true,
+  ignoreCustomComments: [/^#/],
+  minifyCSS: true,
+  minifyJS: true,
+  sortAttributes: true,
+  sortClassName: true
+}
+
+export async function handle({ event, resolve }) {
+  const response = await resolve(event, {
+    transformPageChunk: ({ html }) => minify(html, minification_options)
+  })
+
+  return response
+}
