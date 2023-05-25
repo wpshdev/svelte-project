6e9b63f13feabe1211e61625ba9aeb5e8e2cc6c2
paulwpsh <113500108+paulwpsh@users.noreply.github.com>
2023-05-25 15:57:18 +0800

    fixed overwritten code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,11 @@
     import Footer from "./Footer.svelte";
     import Header from "./Header.svelte";
 	import { redirect } from "@sveltejs/kit";
-	import { Container } from "sveltestrap";
+    import {page} from '$app/stores'
+    // let isHomePage = false;
+    function isHome(url) {
+        return url === "/" ? false : true;
+    }    
     let yaxis: any;
     export let data;
     </script>
@@ -13,7 +17,7 @@
     </svelte:head>    
     <div class="z10">
         <Headroom on:pin={onPin} duration="400ms" offset={50} tolerance={5}>  
-            <header class="d-block" class:changeBG={yaxis >= 250}>
+            <header class="d-block" class:changeBG={yaxis >= 250 || isHome($page.url.pathname)} >
                 <Container>
                     <Header menu={data} />
                 </Container>
