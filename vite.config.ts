7948ad53fb62959c357064199ee48acdfaa5b035
paulwpsh <113500108+paulwpsh@users.noreply.github.com>
2023-05-25 15:27:52 +0800

    fixed issues caused by bharat's code

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const filePath = dirname(fileURLToPath(import.meta.url))
+const sassPath = `${filePath}/src/styles/`
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
+	// test: {
+	// 	include: ['src/**/*.{test,spec}.{js,ts}']
+	// },
+	css: {
+		preprocessorOptions: {
+		  scss: {
+			additionalData: `
+				@import '${sassPath}_mixins.scss';
+				@import '${sassPath}_vars.scss';
+			`,
+		  }
+		},
+	},	
+	
 	build:{
-		sourcemap: true // Config vite to generate sourcemap when bundling.
+		sourcemap: true, // Config vite to generate sourcemap when bundling.
+		minify: true
 	},
 };
 
