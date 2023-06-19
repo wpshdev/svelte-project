1696c93fd3b9410e9dc592297e334808bbc54544
parthwps <parth@netdevs.net>
2023-06-20 03:29:15 +0530

    Update PortfolioCarousel.svelte

--- src/lib/components/layout/PortfolioCarousel.svelte
+++ src/lib/components/layout/PortfolioCarousel.svelte
@@ -21,17 +21,17 @@
       onUpdate: (self) => {
         const progress = self.progress;
         // Container width
-        const conwidth = -(container.scrollWidth) + (window.innerWidth * 1.75);
+        const conwidth = -(container.scrollWidth) + (window.innerWidth * 0.75);
         // Position of Container scrolling
         const conx = (-container.scrollWidth * progress);
-        // console.log(" ConWidth: " + conwidth + " ConX: " + conx);
-        // console.log("window.innerWidth: "+ window.innerWidth);
-        // console.log("container.scrollWidth: "+ container.scrollWidth);
+        console.log(" ConWidth: " + conwidth + " ConX: " + conx);
+        console.log("window.innerWidth: "+ window.innerWidth);
+        console.log("container.scrollWidth: "+ container.scrollWidth);
           if(conx < conwidth){
             return
           }else{
             gsap.to(container, {
-            x: (-container.scrollWidth * progress) - window.innerWidth,
+            x: (-container.scrollWidth * progress),
             duration: 0.01,
           });
         }
