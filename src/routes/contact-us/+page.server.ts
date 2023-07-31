9b06b721d89a4a6c91eb4e7672203364be47615e
david <david@netdevs.net>
2023-07-31 18:19:43 +0800

    Fix contact component, made email to, subject, response dynamic

--- src/routes/contact-us/+page.server.ts
+++ src/routes/contact-us/+page.server.ts
@@ -1,10 +1,25 @@
 import { STRAPI_API } from '$env/static/private';
+import axios from '$lib/axios';
 
 export async function load() {
     const url = 'https://strapi.ulfbuilt.com:1337/api/contact-page?populate=*';
-    const response = await fetch(url, {
-        headers: { 'Authorization': 'bearer ' + STRAPI_API },
-    });
-    return response.json();
+
+    const headers = {
+        Authorization: 'Bearer ' + STRAPI_API 
+    }  
+    const response = await axios.get(url, { headers });
+
+    const contactDetailsURL = 'https://strapi.ulfbuilt.com:1337/api/site-setting?populate=deep,3';
+    const contactDetails = await axios.get(contactDetailsURL, { headers }); 
+
+    // const response = await fetch(url, {
+    //     headers: { 'Authorization': 'bearer ' + STRAPI_API },
+    // });
+    // return response.json();
+
+    return {
+        contact: response.data,
+        contactDetails: contactDetails.data,
+    }
 }
 
