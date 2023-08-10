24615aec3965ea04a2e230d7f2bbbbc36a52a8d6
david <david@netdevs.net>
2023-08-10 15:58:37 +0800

    8-10-23 Animation Update

--- src/lib/GsapAnimation.js
+++ src/lib/GsapAnimation.js
@@ -6,6 +6,8 @@
 // Dynamic attributes
 // Add 'gsap-delay' on the element to add delay in seconds. e.g gsap-delay="1" (1s delay)
 // Add 'gsap-duration' on the element to add duration in seconds. e.g gsap-duration="1" (1s duration)
+// Add 'gsap-y' on the element to add starting position of fly in
+// Add 'gsap-x' on the element to add starting position of slide in
 
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
@@ -16,6 +18,8 @@ gsap.registerPlugin(ScrollTrigger);
 
 const delayDefault = '0';
 const durationDefault = '1';
+const flyY = '70';
+const slideX = '7';
 
 // FadeIn
 
@@ -25,7 +29,7 @@ export function fadeIn(node) {
     const targetElementID = targetElement.id;
     const delay = targetElement.getAttribute("gsap-delay") ? targetElement.getAttribute("gsap-delay") : delayDefault;
     const duration = targetElement.getAttribute("gsap-duration") ? targetElement.getAttribute("gsap-duration") : durationDefault;
-
+    
     // FadeIn animation
     ScrollTrigger.create({
         trigger: '#' + targetElementID,
@@ -51,17 +55,18 @@ export function fadeIn(node) {
 
 }
 
-// FlyUp
+// Fly
 
-export function flyUp(node) {
+export function fly(node) {
 
     const targetElement = node; // target element
     const targetElementID = targetElement.id;
     const delay = targetElement.getAttribute("gsap-delay") ? targetElement.getAttribute("gsap-delay") : delayDefault;
     const duration = targetElement.getAttribute("gsap-duration") ? targetElement.getAttribute("gsap-duration") : durationDefault;
+    const y = targetElement.getAttribute("gsap-y") ? targetElement.getAttribute("gsap-y") : flyY;
     targetElement.style.opacity = '0';
 
-    // FlyUp animation
+    // Fly animation
 
     ScrollTrigger.create({
         trigger: '#' + targetElementID,
@@ -71,15 +76,15 @@ export function flyUp(node) {
                 '#' + targetElementID,
                 {
                     opacity: 0,
-                    yPercent: 70,
+                    yPercent: y,
                 }
             );
             tl.to(
                 '#' + targetElementID,
                 {
                     duration: duration,
-                    yPercent: 0,
                     opacity: 1,
+                    yPercent: 0,
                     delay: delay,
                 }
             ); 
@@ -87,7 +92,47 @@ export function flyUp(node) {
     })
 }
 
+
+// Slide
+
+export function slide(node) {
+
+    const targetElement = node; // target element
+    const targetElementID = targetElement.id;
+    const delay = targetElement.getAttribute("gsap-delay") ? targetElement.getAttribute("gsap-delay") : delayDefault;
+    const duration = targetElement.getAttribute("gsap-duration") ? targetElement.getAttribute("gsap-duration") : durationDefault;
+    const x = targetElement.getAttribute("gsap-x") ? targetElement.getAttribute("gsap-x") : slideX;
+    targetElement.style.opacity = '0';
+
+    // slide animation
+
+    ScrollTrigger.create({
+        trigger: '#' + targetElementID,
+        // markers: true,
+        onEnter: function() { 
+            const tl = gsap.timeline();
+            tl.set(
+                '#' + targetElementID,
+                {
+                    opacity: 0,
+                    xPercent: x,
+                }
+            );
+            tl.to(
+                '#' + targetElementID,
+                {
+                    duration: duration,
+                    xPercent: 0,
+                    opacity: 1,
+                    delay: delay,
+                }
+            ); 
+        },
+    });
+}
+
 // Text Animate
+// Add text-animate class on the element
 
 export function textAnimate(node) {
 
@@ -101,13 +146,14 @@ export function textAnimate(node) {
     const charSplit = text.split('');
     let splittedHTML = '';
     charSplit.forEach((c)=> {
-      splittedHTML += `<span> ${c === ' ' ? '&nbsp' : c} </span>`;
+      splittedHTML += `<span>${c === ' ' ? '&nbsp' : c}</span>`;
     })
     targetElement.innerHTML = splittedHTML;
 
     // Text animation
     ScrollTrigger.create({
         trigger: '#' + targetElementID,
+        // markers: true,
         onEnter: function() { 
             const tl = gsap.timeline();
             let letter = targetElement.querySelectorAll('span');
