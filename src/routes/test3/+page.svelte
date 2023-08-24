2366305863316726a3274f2a25cd927a93a55979
parthwps <parth@netdevs.net>
2023-08-24 18:48:48 +0530

    Update +page.svelte

--- src/routes/test3/+page.svelte
+++ src/routes/test3/+page.svelte
@@ -1,65 +1,66 @@
 <script>
-    import { onMount } from "svelte";
-    import { scrollTrigger } from "gsap/dist/scrolltrigger";
-    
-    let images = [
-      {
-        src: "https://picsum.photos/2000/1000",
-      },
-      {
-        src: "https://picsum.photos/2000/1000?grayscale",
-      },
-      {
-        src: "https://picsum.photos/2000/1000?blur=5",
-      },
-    ];
-    
-    let container = document.querySelector(".container");
-    
-    onMount(() => {
-      images.forEach((image) => {
-        const { src } = image;
-        const imageEl = document.createElement("img");
-        imageEl.src = src;
-        container.appendChild(imageEl);
-      });
-    
-      scrollTrigger(".container", {
-        onEnter: () => {
-          images.forEach((image) => {
-            image.el.style.opacity = 1;
-          });
-        },
-        onLeave: () => {
-          images.forEach((image) => {
-            image.el.style.opacity = 0;
-          });
+  import { onMount } from 'svelte';
+  import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+  import { gsap } from 'gsap';
+
+  export let target;
+  export let scrollOffset;
+  export let image;
+  
+  onMount(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    // Create a ScrollTrigger that will trigger when the `target` element enters or leaves the viewport.
+    new ScrollTrigger({
+      trigger: target,
+      start: 'bottom - scrollOffset',
+      end: 'bottom',
+
+    });
+
+    const sections = document.querySelectorAll('.target');
+    sections.forEach((section, index) => {
+      let translateY = -300; // Initial translation value
+      const translationSpeed = 5; // Adjust the speed of the translation
+      
+      gsap.to(section, {
+        y: () => translateY,
+        scrollTrigger: {
+          markers:true,
+          trigger: section,
+          start: 'top bottom', // Trigger animation when the top of the section reaches the bottom of the viewport
+          end: 'bottom top',   // Trigger animation when the bottom of the section reaches the top of the viewport
+          scrub: true,         // Smoothly animate the translation as the user scrolls
+          onUpdate: (self) => {
+            translateY = self.progress * window.innerHeight * translationSpeed; // Calculate the translation value with speed adjustment
+          },
         },
       });
     });
-    </script>
+
     
-    <style>
-    .container {
-      height: 100vh;
+  });
+  </script>
+  
+  <style>
+    .container{
+      width: 30rem;
+      height: 30rem;
       overflow: hidden;
+      background-color: red;
     }
-    
-    .image {
-      width: 100%;
-      height: 100vh;
-      opacity: 0;
+    .target{
+      width:200%;
+      height: 200%;
+      margin-left: -15%;
+      margin-top: -100px;
+      background-position: center;
+      background-size: cover;
     }
-    </style>
-    
-    <div class="container">
-      <div class="image" data-gs-parallax="0">
-        <img src="https://picsum.photos/2000/1000" />
-      </div>
-      <div class="image" data-gs-parallax="0">
-        <img src="https://picsum.photos/2000/1000?grayscale" />
-      </div>
-      <div class="image" data-gs-parallax="0">
-        <img src="https://picsum.photos/2000/1000?blur=5" />
-      </div>
+  </style>
+  
+  <div style="height:150vh;"></div>
+  <div class="container">
+    <div class="target" style="background-image: url('https://picsum.photos/2000/1000');">
     </div>
+  </div>
+  <div style="height:60vh;"></div>
