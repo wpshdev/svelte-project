4d2b748e7f8c870c00d175f2ae9f73e6dae3d20c
parthwps <parth@netdevs.net>
2023-11-10 22:10:29 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,7 +43,7 @@
   import {navigating} from '$app/stores'
 
   // NProgress css
-  import 'nprogress/nprogress.css'
+//   import 'nprogress/nprogress.css'
 
   NProgress.configure({
         minimum: 0.16,
