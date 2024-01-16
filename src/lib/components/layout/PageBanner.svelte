93958c0c5f4b699df071d53aabd3bd9c17670d9b
parthwps <parth@netdevs.net>
2024-01-16 23:43:34 +0530

    overlay remved

--- src/lib/components/layout/PageBanner.svelte
+++ src/lib/components/layout/PageBanner.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <section in:fadeIn id="banner_bg" class="banner {extraClass ? extraClass : ''} {transparent ? transparent : 'transparent'} autoscroll-exception" style="--banner: url({pageBanner}); height: {pageBannerheight}vh;" rel = "preload">
-	<div class="banner_overlay" in:fadeOut id="banner_overlay" gsap-duration="0" gsap-delay="0"></div>
+	<!-- <div class="banner_overlay" in:fadeOut id="banner_overlay" gsap-duration="0" gsap-delay="0"></div> -->
 	<Container>
 		<Row>
 			<Col>
