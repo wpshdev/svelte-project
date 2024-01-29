d16b84820a4d5adbc8a6a8daf83eb4706ab7504d
parthwps <parth@netdevs.net>
2024-01-30 00:23:19 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -64,40 +64,31 @@
       NProgress.done();
     }
   }
+
+  onMount(() => {
+        const script = document.createElement('script');
+        script.src = `https://www.googletagmanager.com/gtm.js?id=GTM-MQZ96BM9`;
+        script.async = true;
+        document.head.appendChild(script);
+
+        const noscript = document.createElement('noscript');
+        noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQZ96BM9" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+        document.body.appendChild(noscript);
+    });
+  onMount(() => {
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = `https://www.googletagmanager.com/gtag/js?id=G-7CK4C17W36`;
+        document.head.appendChild(script);
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-7CK4C17W36');
+  });
     </script>
     <svelte:head>
         <link rel="preconnect" href="https://api.ulfbuilt.com/">
-        <!-- Google Analytics Code -->
-  <!-- <script>
-    import { onMount } from 'svelte';
-    import { page } from '$stores'; // Assuming you have a reactive store for tracking page views
-
-    onMount(() => {
-        if(data.fallback.data.attributes.Google_Analytics){
-            gtag('config', 'data.fallback.data.attributes.Google_Analytics', { page_path: '/' + $page });
-        }
-    });
-  </script>
-
-  <script context="module">
-    export const page = writable('');
-  </script>
-
-  <script>
-    // Replace 'GTM-XXXXXXX' with your actual container ID
-    if(data.fallback.data.attributes.Google_Tags){
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', data.fallback.data.attributes.Google_Tags);
-    }
-  </script> -->
     </svelte:head>
     
     <div class="z10 {isPortfolio($page.route.id)} {isArticle($page.route.id)}">
