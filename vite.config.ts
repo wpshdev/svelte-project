baa87b9565aa3c82fcd7f007e7462158d3a2c2ac
parthwps <parth@netdevs.net>
2024-01-31 03:03:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,31 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const filePath = dirname(fileURLToPath(import.meta.url))
-// I think there is something wrong with this path -- Ryan Iguchi - 6/28/2023
+// Determine the directory path of the current file
+const filePath = dirname(fileURLToPath(import.meta.url));
 
-// This is another possibility for this filePath var
-// const filePath = process.cwd()
-
-
-// I think it is the sassPath that is causing the File error. 
-// How do we trace this out and rewrite the path to the local file path?
-// root: process.cwd() ------ This is the suggested path constant.
-
-// const sassPath = `D:/Coding - David/Work/NetDevs/ULF/src/styles/`
-const sassPath = `${filePath}/src/styles/`
+// Path to the directory containing SCSS files
+const sassPath = `${filePath}/src/styles/`;
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	// test: {
-	// 	include: ['src/**/*.{test,spec}.{js,ts}']
-	// },
-	css: {
-		preprocessorOptions: {
-		  scss: {
-			additionalData: `
-				@import '${sassPath}_mixins.scss';
-				@import '${sassPath}_vars.scss';
-			`,
-		  }
-		},
-	},	
-	
-	build:{
-//		root: process.cwd(), ------- Going to leave this commented out for now. 
-		sourcemap: true, // Config vite to generate sourcemap when bundling.
-		minify: true
-	},
+  plugins: [sveltekit()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import '${sassPath}_mixins.scss';
+          @import '${sassPath}_vars.scss';
+        `,
+      },
+    },
+  },
+  build: {
+    // root: process.cwd(), // Uncomment and set to project root if necessary
+    sourcemap: true, // Generate sourcemaps for debugging
+    minify: true, // Minify output for production
+  },
 };
 
 export default config;
