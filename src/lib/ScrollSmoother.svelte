32e9f4e57c9a0afc3fa7dc923f121969552f46c0
parthwps <parth@netdevs.net>
2024-02-01 02:06:16 +0530

    new ss

--- src/lib/ScrollSmoother.svelte
+++ src/lib/ScrollSmoother.svelte
@@ -15,55 +15,30 @@ console.log('Is desktop device:', isDesktopDevice());
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin);
     
     onMount(() => {
-
-
-
-// Function to handle scroll events
-function handleScroll() {
-    const scrollX = window.scrollX || window.pageXOffset; // Horizontal scroll position
-    const scrollY = window.scrollY || window.pageYOffset; // Vertical scroll position
-
-    // console.log("Scroll X:", scrollX);
-    // console.log("Scroll Y:", scrollY);
-}
-
-// Function to handle touch events
-function handleTouchMove(event) {
-    const touchX = event.touches[0].clientX; // Horizontal touch position
-    const touchY = event.touches[0].clientY; // Vertical touch position
-
-    // console.log("Touch X:", touchX);
-    // console.log("Touch Y:", touchY);
-}
-
-// Add event listeners for scroll and touchmove events
-window.addEventListener('scroll', handleScroll);
-window.addEventListener('touchmove', handleTouchMove);
-
-      if(isMobileDevice() == true) {
-        smoothValue = 0;
-        smoothTouchValue = 0;
-      } else {
-        smoothValue = 2;
-        smoothTouchValue = 1.5;
-      }
-      window.addEventListener('resize', () => {
-        if(isMobileDevice() == true) {
-          smoothValue = 0;
-          smoothTouchValue = 0;
-        } else {
-          smoothValue = 2;
-          smoothTouchValue = 1.5;
-        }
-      });
+      // if(isMobileDevice() == true) {
+      //   smoothValue = 0;
+      //   smoothTouchValue = 0;
+      // } else {
+      //   smoothValue = 2;
+      //   smoothTouchValue = 1.5;
+      // }
+      // window.addEventListener('resize', () => {
+      //   if(isMobileDevice() == true) {
+      //     smoothValue = 0;
+      //     smoothTouchValue = 0;
+      //   } else {
+      //     smoothValue = 2;
+      //     smoothTouchValue = 1.5;
+      //   }
+      // });
 
       const smoother = ScrollSmoother.create({
         wrapper: "#wrapper",
         content: "#content",
-        smooth: smoothValue,
+        smooth: 2,
         effects: true,
         normalizeScroll: true,
-        smoothTouch: smoothTouchValue,
+        smoothTouch: 1.5,
       });
     });
 
